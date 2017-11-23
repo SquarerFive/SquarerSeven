c0bbee22872a3b284d51b95ed003f3cb7aa7ecfb
SquarerFive <aidensoedjarwo@gmail.com>
2017-11-23 14:51:02 +1100

    Redacted private IDs

--- SquarerSeven.py
+++ SquarerSeven.py
@@ -5,8 +5,7 @@ import json
 import requests
 import apiai
 client = discord.Client()
-user = 'iX7bgNY7pj4BBLsb'
-key = 'Rs1syy4IJqRnufruCtR07zu6ZwiIx0Pz'
+
 CLIENT_ACCESS_TOKEN = 'b6d5e65c84a84d8b805fcdc276d2c57f'
 @client.event
 async def on_ready():
@@ -45,6 +44,6 @@ async def on_message(message):
         await client.send_message(message.channel, word)
 
 print('Starting...')
-#requests.post('https://cleverbot.io/1.0/create', json={'user':user, 'key':key, 'nick':'frost'})
-client.run('MzgwOTM4NzYzMDIwNDAyNjk4.DO_4ZA.-KhSCrO0gcMYjc0T3reLaEY7uvk')
+
+client.run('ENTER YOUR ID HERE')
 quit()
